0de721ac86218551830ad9ce628986528343f683
reshadf <reshadfar@gmail.com>
2012-07-19 23:36:37 +0200

    added new background and changed menu

--- index-body.php
+++ index-body.php
@@ -2,7 +2,7 @@
   <!--[if lt IE 7]><p class=chromeframe>Your browser is <em>ancient!</em> <a href="http://browsehappy.com/">Upgrade to a different browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to experience this site.</p><![endif]-->
   <header>
     <div id="webtitle"> <a href="index.html"><h1><strong>Reshad</strong> Farid</h1></a> </div>
-    <nav role="navigation" class="navigation" id="navigation">
+    <nav role="navigation" class="navigation">
             <?php
             echo '
                    <ul>
--- main.php
+++ main.php
@@ -13,17 +13,17 @@ if(!isset($_GET['id']))
           <fieldset id="user-details">  
             
             <label for="name">Naam:</label>
-            <input type="text" name="name" value="" /> 
+            <input type="text" name="name" value="" placeholder="Vul uw naam in" /> 
           
             <label for="email">Email:</label> 
-            <input type="email" name="email" value=""  /> 
+            <input type="email" name="email" value=""  placeholder="Vul uw email adress in"/> 
           
           </fieldset>
           
           <fieldset id="user-message">
           
             <label for="message">Uw bericht:</label> 
-            <textarea name="message" rows="0" cols="0"></textarea> 
+            <textarea name="message" rows="0" cols="0" placeholder="Vul hier uw bericht in"></textarea> 
           
             <input type="submit" value="Verzenden" name="submit" class="submit" />   
           

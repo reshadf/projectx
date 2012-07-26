9afba992ecd9995a83900ef1d3ac7a88b613e1c2
reshadf <reshadfar@gmail.com>
2012-07-26 14:04:32 +0200

    modified container and css

--- portfolio.php
+++ portfolio.php
@@ -1,4 +1,6 @@
-<section class="main">
-	<h1>Binnenkort Online</h1>
-<p>Vanwege onderhoud zijn sommige pagina's nog niet online.</p>
-</section>
+<div class="container" role="main">
+	<section class="main">
+		<h1>Binnenkort Online</h1>
+	<p>Vanwege onderhoud zijn sommige pagina's nog niet online.</p>
+	</section>
+</div>

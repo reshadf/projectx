aca9a199aefd14021d0f71064528da6a440584d1
Reshad Farid <reshadfar@gmail.com>
2012-07-31 17:43:19 +0200

    admin changes

--- admin/admin.php
+++ admin/admin.php
@@ -6,7 +6,96 @@ $username = $_SESSION['username'];
 
 if($_SERVER['REQUEST_METHOD']== 'POST'){
 	
-}
+	if(isset($_POST['personal']))
+	{
+		try 
+		{
+		  $dbUpdate = new PDO('mysql:host=localhost;dbname=projectx', 'root', 'root');
+		  $dbUpdate->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+		    $stmtUpdate = $dbUpdate->prepare('  SELECT 
+		                                member_id, username, password, gender, age, permission
+		                            FROM  
+		                                members
+		                        	WHERE 
+		                        		username = :username
+		                        ');
+
+		    $stmtUpdate->bindParam(':username', $username, PDO::PARAM_STR);
+
+		    $stmtUpdate->execute();
+		    
+		    $formUpdate = '';
+		    if($stmtUpdate === false)
+		    {
+		      $formUpdate = 'error 01';
+		    }
+		    else
+		    {
+
+		       while($row = $stmtUpdate->fetch(PDO::FETCH_ASSOC))
+			      {
+			      	$formUpdate .= '<form method="post" class="block">';
+			        $formUpdate .= '<label>Member:</label> 16121992' . $row['member_id'] . '<br><br>';
+			        $formUpdate .= '<label>Name:</label> <input type="text" value="' . $row['username'] . '"><br>';
+			        $formUpdate .= '<label>Password:</label> <input type="password" value=""<br><br>';
+			        $formUpdate .= '<label>Re-Typ Password:</label> <input type="password" value=""<br><br>';
+			        $formUpdate .= '<input type="submit" name="change" value="change settings">';
+			        $formUpdate .= '</form>';
+			        
+			      }
+
+		    }
+		    
+		} 
+		catch (PDOException $e) 
+		{
+		  $formUpdate = "Error:" . $e;
+		}
+
+		  $dbUpdate = NULL;
+	}
+
+	if(isset($_POST['change']))
+	{
+		try 
+		{
+		  $dbChange = new PDO('mysql:host=localhost;dbname=projectx', 'root', 'root');
+		  $dbChange->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+		    $stmtUpdateIt = $dbUpdate->prepare('  UPDATE 
+		                                 members
+		                            SET  
+		                                username = , password = 
+
+		                        	WHERE 
+		                        		username = :username
+		                        ');
+
+		    $stmtUpdateIt->bindParam(':username', $username, PDO::PARAM_STR);
+
+		    $stmtUpdateIt->execute();
+		    
+		    
+		    if($stmtUpdateIt === false)
+		    {
+		      $updateMsg = 'error 01';
+		    }
+		    else
+		    {
+		    	$updateMsg = 'instellingen gewijzigd!'; 
+		    }
+		    
+		} 
+		catch (PDOException $e) 
+		{
+		  $formUpdate = "Error:" . $e;
+		}
+
+		  $dbUpdate = NULL;
+	}
+
+} // end request method post
 
  	try 
 	{
@@ -103,14 +192,21 @@ if($_SERVER['REQUEST_METHOD']== 'POST'){
  	<h3>Configuration</h3>
  	<hr>
  	<section class="managers">
-	 	<ul>
-	 		<li><a href="#">Manage articles</a></li>
-	 		<li><a href="#">Personal settings</a></li>
-	 	</ul>
+ 		<form method="post">
+		 	<ul>
+		 		<li><input type="submit" name="articles" value="Manage articles"></li>
+		 		<li><input type="submit" name="personal" value="Personal"></li>
+		 	</ul>
+	 	</form>
  	</section>
   </aside>
   <section class="main" >
-  	
+  		<?php
+  			if(isset($formUpdate))
+  			{
+  				echo $formUpdate;
+  			}
+  		?>
   </section>
 
   <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js" type="text/javascript"></script>

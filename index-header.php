33468959b752d84606abb5163c793cc5d88216a0
Reshad Farid <reshadfar@gmail.com>
2012-08-09 16:58:24 +0200

    added portfolio gallery

--- index-header.php
+++ index-header.php
@@ -127,8 +127,8 @@ if (file_exists("index.ini") && is_array($title = parse_ini_file("index.ini", tr
   <meta name="viewport" content="width=device-width">
   <link rel="stylesheet" href="css/style.css">
   <link rel="stylesheet" href="css/design.css">
-  
-  <script src="js/libs/modernizr-2.5.3.min.js"></script>
+
+  <script src="//cdnjs.cloudflare.com/ajax/libs/modernizr/2.5.3/modernizr.min.js" type="text/javascript"></script>
 
 <!-- Begin Cookie Consent plugin by Silktide - http://silktide.com/cookieconsent -->
 <link rel="stylesheet" type="text/css" href="css/cookiebar.css"/>

6836f6bde605fed31012aeb79ae6fbfdfaa9a0e1
Reshad Farid <reshadfar@gmail.com>
2012-08-28 22:06:11 +0200

    changed few files for the design

--- about.php
+++ about.php
@@ -1,9 +1,77 @@
-<div class="container" role="main">
-	<article class="main">
-		<h1>Binnenkort online!</h1>
-		<p></p>
-	</article>
-	<article class="contentblock">
+	<section class="ff-container main">
+			
+		<input id="select-type-all" name="radio-set-1" type="radio" class="ff-selector-type-all" checked="checked" />
+		<label for="select-type-all" class="ff-label-type-all">Alles</label>
+		
+		<input id="select-type-1" name="radio-set-1" type="radio" class="ff-selector-type-1" />
+		<label for="select-type-1" class="ff-label-type-1">Web Design</label>
+		
+		<input id="select-type-2" name="radio-set-1" type="radio" class="ff-selector-type-2" />
+		<label for="select-type-2" class="ff-label-type-2">Illustratie</label>
+		
+		<input id="select-type-3" name="radio-set-1" type="radio" class="ff-selector-type-3" />
+		<label for="select-type-3" class="ff-label-type-3">Icon Design</label>
+		
+		<div class="clr"></div>
+		
+		<ul class="ff-items">
+			<li class="ff-item-type-2">
+				<a href="#">
+					<span>Coming soon</span>
+					<img src="img/portfolio.jpg" />
+				</a>
+			</li>
+			<li class="ff-item-type-1">
+				<a href="#">
+					<span>Coming soon</span>
+					<img src="img/portfolio.jpg" />
+				</a>
+			</li>
+			<li class="ff-item-type-1">
+				<a href="#">
+					<span>Coming soon</span>
+					<img src="img/portfolio.jpg" />
+				</a>
+			</li>
+			<li class="ff-item-type-1">
+				<a href="#">
+					<span>Coming soon</span>
+					<img src="img/portfolio.jpg" />
+				</a>
+			</li>
+			<li class="ff-item-type-2">
+				<a href="#">
+					<span>Coming soon</span>
+					<img src="img/portfolio.jpg" />
+				</a>
+			</li>
+			<li class="ff-item-type-2">
+				<a href="#">
+					<span>Coming soon</span>
+					<img src="img/portfolio.jpg" />
+				</a>
+			</li>
+			<li class="ff-item-type-3">
+				<a href="#">
+					<span>Coming soon</span>
+					<img src="img/portfolio.jpg" />
+				</a>
+			</li>
+			<li class="ff-item-type-1">
+				<a href="#">
+					<span>Coming soon</span>
+					<img src="img/portfolio.jpg" />
+				</a>
+			</li>
+			<li class="ff-item-type-3">
+				<a href="#">
+					<span>Coming soon</span>
+					<img src="img/portfolio.jpg" />
+				</a>
+			</li>
+		</ul>
+	</section>
+		<article class="contentblock">
 		<h2>The Story</h2>
 		<p>
 			Mijn naam is <span class="orange">Reshad Farid</span> ik ben <span class="orange">19</span> jaar jong en ik zit op de 
@@ -13,4 +81,9 @@
 			naar mijn <span class="orange"><a href="index.php?page=portfolio.php">portfolio</a></span> kijken om te zien wat ik tot nu toe gerealiseerd heb.
 		</p>
 	</article>
-</div>
+	<article class="contentblock">
+		<h2>The Portfolio</h2>
+		<p>
+			Hiernaast vind u een kleine greep uit mijn portfolio. Vanwege de bouw van mijn nieuwe website kan het zijn dat er nog projecten missen of niet geplaatst zijn.
+		</p>
+	</article>

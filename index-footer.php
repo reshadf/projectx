8087c61b569d15ff6056de371a5616b256b2965a
Reshad Farid <reshadfar@gmail.com>
2012-10-25 21:36:46 +0200

    modified canvas

--- index-footer.php
+++ index-footer.php
@@ -7,61 +7,6 @@
   <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
   <script>window.jQuery || document.write('<script src="js/libs/jquery-1.7.1.min.js"><\/script>')</script>
 
- <script>
-  $(function () {
-
-            //variables
-            var canvas = document.getElementById("chelseaLogo");
-            var CanvasContext = canvas.getContext("2d");
-            var centerX = canvas.width / 2;
-            var centerY = canvas.height / 2;
-            var radius = 170;
-            var radiusWhite = radius - 50;
-
-            // outside logo
-            CanvasContext.beginPath();
-            CanvasContext.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
-            CanvasContext.fillStyle = '#0B49DB';
-            CanvasContext.fill();
-
-            //stroke
-            CanvasContext.lineWidth = 4;
-            CanvasContext.strokeStyle = '#EDD202';
-            CanvasContext.stroke();
-
-            //white innercircle
-            CanvasContext.beginPath();
-            CanvasContext.arc(centerX, centerY, radiusWhite, 0, 2 * Math.PI, false);
-            CanvasContext.fillStyle = '#fff';
-            CanvasContext.fill();
-
-            //stroke
-            CanvasContext.lineWidth = 4;
-            CanvasContext.strokeStyle = '#EDD202';
-            CanvasContext.stroke();
-
-            //text top
-            CanvasContext.font = '35pt Calibri';
-            CanvasContext.fillText('Chelsea', 115, 150);
-
-            //stroke 
-            CanvasContext.strokeStyle = '#EDD202';
-            CanvasContext.lineWidth = 1;
-            CanvasContext.strokeText('Chelsea', 115, 150);
-
-            //text bottom
-            CanvasContext.font = '30pt Calibri';
-            CanvasContext.fillText('Football Club', 85, 200);
-
-            //stroke
-            CanvasContext.strokeStyle = '#EDD202';
-            CanvasContext.lineWidth = 1;
-            CanvasContext.strokeText('Football Club', 85, 200);
-       
-});
-
- </script>
-
   <script src="js/plugins.js"></script>
   <script src="js/script.js"></script>
 
--- play.php
+++ play.php
@@ -1,5 +1,77 @@
 <section class="main">
-<canvas id="chelseaLogo" width="380" height="350">  
+<canvas id="chelseaLogo" width="400" height="400">  
 <b>This browser does not support HTML5 Canvas!</b>  
 </canvas>
+<script>
+var canvas = document.getElementById("chelseaLogo");
+var CanvasContext = canvas.getContext("2d");
+
+var canvas = document.getElementById("chelseaLogo");
+            var CanvasContext = canvas.getContext("2d");
+            var centerX = canvas.width / 2;
+            var centerY = canvas.height / 2;
+            var radius = 180;
+            var radiusWhite = radius - 55;
+
+            // outside logo
+            CanvasContext.beginPath();
+            CanvasContext.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
+            CanvasContext.fillStyle = '#0B49DB';
+            CanvasContext.fill();
+
+            //stroke
+            CanvasContext.lineWidth = 4;
+            CanvasContext.strokeStyle = '#EDD202';
+            CanvasContext.stroke();
+
+            //white innercircle
+            CanvasContext.beginPath();
+            CanvasContext.arc(centerX, centerY, radiusWhite, 0, 2 * Math.PI, false);
+            CanvasContext.fillStyle = '#fff';
+            CanvasContext.fill();
+
+            //stroke
+            CanvasContext.lineWidth = 4;
+            CanvasContext.strokeStyle = '#EDD202';
+            CanvasContext.stroke();
+
+            // tekst
+function drawTextAlongArc(context, str, centerX, centerY, radius, angle, above) {
+    var met, wid;
+    context.save();
+    context.translate(centerX, centerY);
+    if (!above) {
+        radius = -radius;
+        angle = -angle;
+    }
+    context.rotate(-1 * angle / 2);
+    context.rotate(-1 * (angle / str.length) / 2);
+
+    for (var n = 0; n < str.length; n++) {
+        var char = str[n];
+        met = context.measureText(char);
+        wid = met.width;
+        console.log(met);
+        
+        context.rotate(angle / str.length);
+        context.fillText(char, -wid / 2, -radius);
+        context.strokeText(char, -wid / 2, -radius);
+    }
+    context.restore();
+}
+CanvasContext.font = 'bold 40pt impact';
+CanvasContext.fillStyle = '#fff';
+CanvasContext.strokeStyle = '#E0C91B';
+CanvasContext.lineWidth = 2;
+drawTextAlongArc(CanvasContext, "CHELSEA", 200, 193, 125, Math.PI*3/5, true);
+    
+CanvasContext.font = 'bold 20pt impact';
+drawTextAlongArc(CanvasContext, "FOOTBALL CLUB", 200, 215, 150, Math.PI*3/5, false);
+
+CanvasContext.beginPath();
+CanvasContext.stroke();
+
+
+
+</script>
 </section>
